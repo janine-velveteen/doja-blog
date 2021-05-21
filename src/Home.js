15fd0f945ada6da21328385db9ed1b65d08a8868
Janine M <68258844+janine-velveteen@users.noreply.github.com>
2021-05-21 14:28:29 -0400

    content update

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'Finally moved in :)', body: 'lorem ipsum...', author: 'Janine', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Janine', id: 2 },
+        { title: 'Summer Sips', body: 'lorem ipsum...', author: 'Janine', id: 3 }
     ]);
     
 
